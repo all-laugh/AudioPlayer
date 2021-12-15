351dd37c804c69b7db4ea7ab8a83da32d9dbb087
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2021-12-15 14:36:34 -0500

    Initial Commit

---
+++ AudioPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioPlayer
+//
+//  Created by Xiao Quan on 12/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
